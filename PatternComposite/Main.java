89980b7942f3975a0dc64a645990b7add789d19d
tiago <tiagofreitastjf@outlook.com.br>
2020-09-19 23:00:25 -0300

    teste

--- PatternComposite/Main.java
+++ PatternComposite/Main.java
@@ -1,32 +1,17 @@
-import Composite.Component;
+package PatternComposite;
+
 import SistemaDeArquivos.*;
 
 class Main {
-  public static void main(String[] args) 
-    {/*
-        Pasta nova = new Pasta("Raiz");
-        nova.GetNomePasta();
-
-        Pasta novaSubPasta1 = new Pasta("SubPasta1");
-        Pasta novaSubPasta2 = new Pasta("SubPasta2");
-        Pasta novaSubPasta3 = new Pasta("SubPasta3");
-        Pasta novaSubPasta4 = new Pasta("SubPasta4");
-        Pasta novaSubPasta5 = new Pasta("SubPasta5");
-        nova.AddPasta(novaSubPasta1);
-        nova.AddPasta(novaSubPasta2);
-        nova.AddPasta(novaSubPasta3);
-        nova.AddPasta(novaSubPasta4);
-        nova.AddPasta(novaSubPasta5);
-
-        nova.ListarPastas();*/
-
+    public static void main(String[] args) 
+    {
         try {
             // Creating a component tree
             Component component = new Pasta();
 
             // Adding all accounts of a customer to component
-            component.add(new Pasta("DA001", 100));
-            component.add(new Pasta("DA002", 150));
+            component.add(new Pasta("DR1", 100));
+            component.add(new Pasta("DR2", 150));
 
             Component subComponent = new Pasta();
             component.add(subComponent);
@@ -37,7 +22,7 @@ class Main {
             System.out.println("Total Balance : " + totalBalance);
         }
         catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
