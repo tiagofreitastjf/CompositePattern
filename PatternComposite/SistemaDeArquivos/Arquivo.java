bb65a9eaf8479b570d3ebf5bebcb7742653a1aee
tiago <tiagofreitastjf@outlook.com.br>
2020-09-19 23:01:29 -0300

    teste

--- PatternComposite/SistemaDeArquivos/Arquivo.java
+++ PatternComposite/SistemaDeArquivos/Arquivo.java
@@ -1,17 +1,15 @@
 package SistemaDeArquivos;
 
-import Composite.Component;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Arquivo
+public class Arquivo extends Composite
 {
   private String nome;
   private int tamanho;
 
   protected List<Component> arquivos = new ArrayList<>();
-
+    
   public void add(Component g) {
     arquivos.add(g);
   }
@@ -19,41 +17,12 @@ public class Arquivo
   public void remove(Component g) {
     arquivos.remove(g);
   }
-
-  public Component getChild(int i) {
-    return (Component) arquivos.get(i);
-  }
-
+  
   public float getBalance() {
-    float totalBalance = 0;
-    for (Component f : arquivos) {
-      totalBalance = totalBalance + f.getBalance();
-    }
-    return totalBalance;
+      float totalBalance = 0;
+      for (Component f : list) {
+          totalBalance = totalBalance + f.getBalance();
+      }
+      return totalBalance;
   }
-  /*
-  public Arquivo(String nome)
-  {
-    this.nome = nome;
-  }
-
-  public String GetNomeArquivo()
-  {
-    return nome;
-  }
-
-  public void SetNomeArquivo(String nome)
-  {
-    this.nome = nome;
-  }
-
-  public int GetTamanhoArquivo()
-  {
-    return tamanho;
-  }
-
-  public void SetTamnhoArquivo(int tamanho)
-  {
-    this.tamanho = tamanho;
-  }*/
 }
--- PatternComposite/SistemaDeArquivos/Pasta.java
+++ PatternComposite/SistemaDeArquivos/Pasta.java
@@ -9,11 +9,6 @@ public class Pasta extends Component
   private String nome;
   private int size;
 
-  public Pasta()
-  {
-
-  }
-
   public Pasta(String nome, int tamanho)
   {
     this.nome = nome;
@@ -41,44 +36,4 @@ public class Pasta extends Component
     }
     return totalBalance;
   }
-  /*
-  private String nome;
-  private List<Arquivo> arquivos = new ArrayList<Arquivo>();
-  private List<Pasta> pastas = new ArrayList<Pasta>();
-
-  public Pasta(String nome)
-  {
-    this.nome = nome;
-  }
-
-  public String GetNomePasta()
-  {
-    return nome;
-  }
-
-  public void SetNomePasta(String nome)
-  {
-    this.nome = nome;
-  }
-
-  public void AddPasta(Pasta nova)
-  {
-    pastas.add(nova);
-  }
-
-  public void ListarPastas()
-  {
-    for (int i = 0; i < pastas.size(); i++)
-    {
-      System.out.println(pastas.get(i).GetNomePasta());
-    }
-  }
-
-  public void ListarArquivos()
-  {
-    for (int i = 0; i < arquivos.size(); i++)
-    {
-      System.out.println("Nome: " + arquivos.get(i).GetNomeArquivo() + " Tamanho: " + arquivos.get(i).GetTamanhoArquivo());
-    }
-  }*/
 }
