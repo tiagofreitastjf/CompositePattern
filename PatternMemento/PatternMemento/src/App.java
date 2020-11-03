f8d7710dae3ed77adf41598b06ea4a4610c96f69
tiago <tiagofreitastjf@outlook.com.br>
2020-11-03 20:26:13 -0300

    Rename

--- PatternMemento/PatternMemento/src/App.java
+++ PatternMemento/PatternMemento/src/App.java
@@ -1,21 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Originator originator = new Originator();
-        Shape careTaker = new Shape();
+        Shape shape = new Shape();
         
         originator.setState("#1 Forma quadrada: ■");
-        careTaker.add(originator.saveStateToMemento());
+        shape.add(originator.saveStateToMemento());
         
         originator.setState("#2 Forma circular: O");
         originator.setState("#3 Forma triangular: /_\\");
-        careTaker.add(originator.saveStateToMemento());
+        shape.add(originator.saveStateToMemento());
         
         originator.setState("#4 Forma retangular: █");
         System.out.println("Forma atual: " + originator.getState());		
         
-        originator.getStateFromMemento(careTaker.get(0));
+        originator.getStateFromMemento(shape.get(0));
         System.out.println("Primeira forma salva: " + originator.getState());
-        originator.getStateFromMemento(careTaker.get(1));
+        originator.getStateFromMemento(shape.get(1));
         System.out.println("Segunda forma salva: " + originator.getState());
     }
 }
